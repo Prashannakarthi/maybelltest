d51523e7ea4eeacb5e6f2df642d20628f3d74acc
Prashannakarthi <prashannakarthikeyan17@gmail.com>
2026-07-31 20:06:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@
 // 2. Copy the deployment URL (ends in /exec) and paste it here.
 // ============================================================
 
-export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyayee1M8S3OXc_OITT4nKZmGyg-2c45nvTv_bZcdc77KUiS0tFYEWZvHB_qnnhymBM/exec";
+export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyLjuG_dklHYNttOFyBGC6CZPOKd2pZmdr2UuUDo18FGIUcScXmtUWSqfwPDT7JNOp2/exec";
